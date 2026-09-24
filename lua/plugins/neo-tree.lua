187775d35913f5a353834d1cd781fb8f0ce1ef22
kayleeh <tkalich.93@gmail.com>
2026-09-24 19:21:26 +0200

    Add neo-tree file explorer

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>ee", "<cmd>Neotree toggle<cr>", desc = "Toggle file explorer" },
+    },
+    opts = {},
+  },
+}
